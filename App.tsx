7d9cf29701525e6ba2078b2629afc9a5fbfb8119
dunz <zergcake@gmail.com>
2022-04-03 17:31:38 +0900

    3.3 QueryClientProvider 사용하기

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {RootStack} from './screens/RootStack';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
