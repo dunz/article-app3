dc9e9fa6f27d64e473189c60482eed684f2d7cae
dunz <zergcake@gmail.com>
2022-04-04 12:38:58 +0900

    5.3 useQuery 사용하기 게시글상세 구현하기

--- screens/ArticleScreen.tsx
+++ screens/ArticleScreen.tsx
@@ -1,17 +1,33 @@
 import React, {VFC} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import {useRoute} from '@react-navigation/native';
 import {ArticleScreenRouteProp} from './types';
+import {useQuery} from 'react-query';
+import {getArticle} from '../api/articles';
+import {getComments} from '../api/comments';
 
 export const ArticleScreen: VFC = () => {
   const {params} = useRoute<ArticleScreenRouteProp>();
+  const {id} = params;
+
+  const articleQuery = useQuery(['article', id], () => getArticle(id));
+  const commentsQuery = useQuery(['comments', id], () => getComments(id));
+
+  if (!articleQuery.data || !commentsQuery.data) {
+    return <ActivityIndicator size="large" style={styles.spinner} color="black" />;
+  }
+
   return (
-    <View style={styles.block}>
-      <Text>{params.id}</Text>
+    <View>
+      <Text>{articleQuery.data.title}</Text>
+      <Text>{articleQuery.data.body}</Text>
+      <Text>{commentsQuery.data.length}개의 댓글</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  spinner: {
+    flex: 1,
+  },
 });
