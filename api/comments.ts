5be7d7cbe9f26d913e1df1e996566399aca8f221
dunz <zergcake@gmail.com>
2022-04-04 12:33:01 +0900

    5.2 게시물단건, 댓글리스트 api 함수 작성하기

---
+++ api/comments.ts
@@ -0,0 +1,7 @@
+import {client} from './client';
+import {Comment} from './types';
+
+export const getComments = async (articleId: number) => {
+  const response = await client.get<Comment[]>(`/articles/${articleId}/comments`);
+  return response.data;
+};
