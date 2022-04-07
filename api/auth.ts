b94ae45a96cbc051befc6bbefce38fdf5520ae4c
dunz <zergcake@gmail.com>
2022-04-07 22:51:26 +0900

    6.3 회원 인증 api 함수 만들기

---
+++ api/auth.ts
@@ -0,0 +1,28 @@
+import {client} from './client';
+import {AuthResult, User} from './types';
+
+interface RegisterParams {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface LoginParams {
+  identifier: string;
+  password: string;
+}
+
+export const register = async (params: RegisterParams) => {
+  const response = await client.post<AuthResult>('/auth/local/register', params);
+  return response.data;
+};
+
+export const login = async (params: LoginParams) => {
+  const response = await client.post<User>('/auth/local', params);
+  return response.data;
+};
+
+export const getLoginStatus = async () => {
+  const response = await client.get<User>('/users/me');
+  return response.data;
+};
