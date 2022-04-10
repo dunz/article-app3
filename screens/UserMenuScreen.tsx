f3672e2ca4ed2e1fd68f1e83f41b58bb6c7862d7
dunz <zergcake@gmail.com>
2022-04-10 17:36:08 +0900

    8.4 로그아웃 기능 구현하기

--- screens/UserMenuScreen.tsx
+++ screens/UserMenuScreen.tsx
@@ -3,17 +3,30 @@ import React, {VFC} from 'react';
 import {View} from 'react-native';
 import {MenuItem} from '../components/MenuItem';
 import {RootStackNavigationProp} from './types';
+import {useUserState} from '../contexts/UserContext';
+import {clearToken} from '../api/client';
 
 export const UserMenuScreen: VFC = () => {
   const navigation = useNavigation<RootStackNavigationProp>();
+  const [user, setUser] = useUserState();
 
   const onLogin = () => navigation.navigate('Login');
   const onRegister = () => navigation.navigate('Register');
+  const onLogout = () => {
+    setUser(null);
+    clearToken();
+  };
 
   return (
     <View>
-      <MenuItem onPress={onLogin} name="로그인" />
-      <MenuItem onPress={onRegister} name="회원가입" />
+      {user ? (
+        <MenuItem onPress={onLogout} name="로그아웃" />
+      ) : (
+        <>
+          <MenuItem onPress={onLogin} name="로그인" />
+          <MenuItem onPress={onRegister} name="회원가입" />
+        </>
+      )}
     </View>
   );
 };
