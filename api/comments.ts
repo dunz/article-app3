b33451b7ae499f682f4264ae4e810bd1a626b776
dunz <zergcake@gmail.com>
2022-04-11 19:18:27 +0900

    12 댓글 작성/수정/삭제 기능 구현하기 - api 함수 준비하기

--- api/comments.ts
+++ api/comments.ts
@@ -5,3 +5,21 @@ export const getComments = async (articleId: number) => {
   const response = await client.get<Comment[]>(`/articles/${articleId}/comments`);
   return response.data;
 };
+
+export const writeComment = async (params: {articleId: number; message: string}) => {
+  const {articleId, message} = params;
+  const response = await client.post<Comment>(`/articles/${articleId}/comments`, {message});
+  return response.data;
+};
+
+export const modifyComment = async (params: {articleId: number; message: string; id: number}) => {
+  const {articleId, message, id} = params;
+  const response = await client.put<Comment>(`/articles/${articleId}/comments/${id}`, {message});
+  return response.data;
+};
+
+export const deleteComment = async (params: {articleId: number; id: number}) => {
+  const {articleId, id} = params;
+  await client.delete<Comment>(`/articles/${articleId}/comments/${id}`);
+  return null;
+};
